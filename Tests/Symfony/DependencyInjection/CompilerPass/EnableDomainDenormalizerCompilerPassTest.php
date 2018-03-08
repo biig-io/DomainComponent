d4292d2513cb356ffee1a4ed5988ff1d1ebb887f
Alexis Thinardon <alexis.thinardon@biig.fr>
2018-03-08 16:18:25 +0100

    Add alias for domainDenormalizerDispatcher

--- Tests/Symfony/DependencyInjection/CompilerPass/EnableDomainDenormalizerCompilerPassTest.php
+++ Tests/Symfony/DependencyInjection/CompilerPass/EnableDomainDenormalizerCompilerPassTest.php
@@ -22,14 +22,16 @@ class EnableDomainDenormalizerCompilerPassTest extends TestCase
     public function testItAddsDefinitionServiceForApiPlatformDomainDenormalizer()
     {
         $definition = $this->prophesize(Definition::class);
-        $container  = $this->prophesize(ContainerBuilder::class);
-        $container->getParameter('kernel.bundles')->willReturn(['ApiPlatformBundle'=>true]);
+        $container = $this->prophesize(ContainerBuilder::class);
+        $container->getParameter('kernel.bundles')->willReturn(['ApiPlatformBundle' => true]);
         $container->register(ApiPlatformDomainDenormalizer::class, ApiPlatformDomainDenormalizer::class)->willReturn($definition)->shouldBeCalled();
         $definition->setDecoratedService('api_platform.jsonld.normalizer.item')->willReturn($definition)->shouldBeCalled();
         $definition->addArgument(new Reference(ApiPlatformDomainDenormalizer::class . '.inner'))->willReturn($definition)->shouldBeCalled();
         $definition->addArgument(new Reference('biig_domain.dispatcher'))->willReturn($definition)->shouldBeCalled();
+        $definition->addTag('serializer.normalizer')->willReturn($definition)->shouldBeCalled();
         $definition->setPublic(false)->willReturn($definition)->shouldBeCalled();
 
+        $container->setAlias('biig.domain_normalizer', ApiPlatformDomainDenormalizer::class)->shouldBeCalled();
         $container->register(DomainDenormalizer::class, DomainDenormalizer::class)->shouldNotBeCalled();
 
         $compilerPass = new EnableDomainDenormalizerCompilerPass();
@@ -39,15 +41,17 @@ class EnableDomainDenormalizerCompilerPassTest extends TestCase
     public function testItAddsDefinitionServiceForDomainDenormalizer()
     {
         $definition = $this->prophesize(Definition::class);
-        $container  = $this->prophesize(ContainerBuilder::class);
+        $container = $this->prophesize(ContainerBuilder::class);
         $container->getParameter('kernel.bundles')->willReturn([]);
         $container->register(ApiPlatformDomainDenormalizer::class, ApiPlatformDomainDenormalizer::class)->willReturn($definition)->shouldNotBeCalled();
 
         $container->register(DomainDenormalizer::class, DomainDenormalizer::class)->willReturn($definition)->shouldBeCalled();
         $definition->addArgument(new Reference('serializer.normalizer.object'))->willReturn($definition)->shouldBeCalled();
         $definition->addArgument(new Reference('biig_domain.dispatcher'))->willReturn($definition)->shouldBeCalled();
-        $definition->addTag('serializer.normalizer', ['priority' => 1000])->willReturn($definition)->shouldBeCalled();
+        $definition->addTag('serializer.normalizer')->willReturn($definition)->shouldBeCalled();
         $definition->setPublic(false)->willReturn($definition)->shouldBeCalled();
+
+        $container->setAlias('biig.domain_normalizer', DomainDenormalizer::class)->shouldBeCalled();
 
         $compilerPass = new EnableDomainDenormalizerCompilerPass();
         $compilerPass->process($container->reveal());
