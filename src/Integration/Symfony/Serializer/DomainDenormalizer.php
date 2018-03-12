d81e64e99869e5dd03c4327093fd196ed703c369
Alexis Thinardon <alexis.thinardon@biig.fr>
2018-03-12 15:19:48 +0100

    Refactoring of the deserializer Many issues are fix with this refactoring. The usage is now far more smooth and homogen.

--- src/Integration/Symfony/Serializer/DomainDenormalizer.php
+++ src/Integration/Symfony/Serializer/DomainDenormalizer.php
@@ -5,18 +5,29 @@ namespace Biig\Component\Domain\Integration\Symfony\Serializer;
 use Biig\Component\Domain\Event\DomainEventDispatcher;
 use Biig\Component\Domain\Model\ModelInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
-final class DomainDenormalizer implements DenormalizerInterface
+/**
+ * Class DomainDenormalizer.
+ *
+ * @final in v2.x
+ */
+/* final */ class DomainDenormalizer implements NormalizerInterface, DenormalizerInterface, SerializerAwareInterface
 {
-    use DomainModelDenormalizer;
-
     /**
      * @var ObjectNormalizer
      */
     private $decorated;
 
-    public function __construct(ObjectNormalizer $decorated, DomainEventDispatcher $dispatcher)
+    /**
+     * @var DomainEventDispatcher
+     */
+    private $dispatcher;
+
+    public function __construct(NormalizerInterface $decorated, DomainEventDispatcher $dispatcher)
     {
         $this->decorated = $decorated;
         $this->dispatcher = $dispatcher;
@@ -25,8 +36,45 @@ final class DomainDenormalizer implements DenormalizerInterface
     /**
      * {@inheritdoc}
      */
+    public function denormalize($data, $class, $format = null, array $context = array())
+    {
+        $domain = $this->decorated->denormalize($data, $class, $format, $context);
+
+        if (is_subclass_of($class, ModelInterface::class)) {
+            $domain->setDispatcher($this->dispatcher);
+        }
+
+        return $domain;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function supportsDenormalization($data, $type, $format = null)
     {
-        return class_exists($type) && is_subclass_of($type, ModelInterface::class);
+        return $this->decorated->supportsDenormalization($data, $type, $format);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        return $this->decorated->normalize($object, $format, $context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        if ($this->decorated instanceof SerializerAwareInterface) {
+            $this->decorated->setSerializer($serializer);
+        }
     }
 }
